89c52355ee35bc939bae81368bad1342c83be94e
mariloubylr <marilou.beyeler@gmail.com>
2023-10-25 18:27:18 +0200

    update data on github pages

--- backend/fetch_data.py
+++ backend/fetch_data.py
@@ -1,7 +1,10 @@
 import json
+import os
+import sys
 
 
 if __name__ == "__main__":
-    f = open("ComplexData.json", "a")
+    data_dir = sys.argv[1]
+    f = open(os.path.join(data_dir, "ComplexData.json"), "a")
     f.write(json.dumps({"some": {"very": {"complex": "data"}}}))
     f.close()
