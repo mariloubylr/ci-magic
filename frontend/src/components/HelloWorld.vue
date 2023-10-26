ae7ed82524262857466486887ad9281039d89153
mariloubylr <marilou.beyeler@gmail.com>
2023-10-26 20:34:22 +0200

    build to corresponding output directories

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    {{ data }}
+    <!-- {{ data }} -->
   </div>
 </template>
 
 <script setup lang="ts">
-import data from '../../data_dir/ComplexData.json'
+// import data from '../../data_dir/ComplexData.json'
 defineProps({
   msg: String
 })
